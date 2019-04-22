6c34379ff98d2e3bcce9dc6f1d796a6e04255162
翁士荃 <wengshiquan@wengshiuandembp.resource.campus.njit.edu>
2019-04-22 11:33:18 -0400

    insert faker data into database with seeding file

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -7,6 +7,10 @@ use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->word,
+        'detail' => $faker->paragraph,
+        'price' => $faker->numberBetween(100, 1000),
+        'discount' => $faker->randomDigit,
+        'stock' => $faker->numberBetween(2,30)
     ];
 });
--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -3,10 +3,16 @@
 /* @var $factory \Illuminate\Database\Eloquent\Factory */
 
 use App\Model\Review;
+use App\Model\Product;
 use Faker\Generator as Faker;
 
 $factory->define(Review::class, function (Faker $faker) {
     return [
-        //
+        'product_id' => function(){
+            return Product::all()->random();
+        },
+        'customer' => $faker->name,
+        'text' => $faker->paragraph,
+        'star' => $faker->numberBetween(1,5)
     ];
 });
